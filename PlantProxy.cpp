5c8eded8ce923579b74a91f6abce108db740d3c3
Glonge-Ares <52624843+Glonge-Ares@users.noreply.github.com>
2020-03-23 17:49:38 +0300

    Update PlantProxy.cpp

--- PlantProxy.cpp
+++ PlantProxy.cpp
@@ -14,13 +14,13 @@
 #include <mutex>
 #include <map>
 #include <netinet/tcp.h>
-#define MAX_EVENTS 10000
+#define MAX_EVENTS 10000 //Кол-во евентов в еполле
 
 
 #include "BanList.h"
 
 
-int setnonblocking(int fd)
+int setnonblocking(int fd) //Делает сокет неблокирующим (ОЧЕНЬ ВАЖНО)
 {
 	int flags = fcntl(fd, F_GETFL);
 	
@@ -28,7 +28,7 @@ int setnonblocking(int fd)
 }
 
 
-int socket_connect( const char* ip, int port)
+int socket_connect( const char* ip, int port) //Подключает сокет куда-либо, тут всё просто
 {
 	
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -46,36 +46,38 @@ int socket_connect( const char* ip, int port)
 	
 }
 
-struct Connection
+struct Connection //Это структура, которая привязывается к каждому соединению.
 {
-	uint32_t ip;
-	int src;
-	int dst;
-	int offset = 0;
-	int len = 0;
-	int buf_size = 8190;
-	unsigned char buffer[8192];
+	uint32_t ip;	//IP адрес, откуда идет соединение (только для входящих)
+	int src;	//файловый дескриптор сокета ЭТОГО соединения
+	int dst;	//файловый дескриптор сокета соединения ПОЛУЧАТЕЛЯ
+	int offset = 0;	//То, до какого байта из буфера были отправлены данные
+	int len = 0;	//Сколько еще надо отправить байт данных
+	int buf_size = 8191;	//Размер буффера (указан чуть меньше, во избежание Seg Fault)
+	unsigned char buffer[8192]; 	//Буффер
 	
-	int packet_offset = 0;
-	int packet_len = 0;
-	bool is_server = false;
-	bool is_handshaked = false;
+	//Далее идут данные для декодера пакетов
+	
+	int packet_offset = 0;		//До какого байта был прочитан пакет
+	int packet_len = 0;		//Какую длину пакета осталось прочитать
+	bool is_server = false;		//Является ли соединение исходящим от сервера?
+	bool is_handshaked = false;	//Был ли валидный хендшейк?
 };
 
 
-
+/*Не используется, листай дальше
 class Redirect
 {
 	public:
 		std::string ip;
 		int port;
-};
+};*/
 
 
 
 
 
-int readVarInt(Connection* dst, int& bytes_read, int correction) {
+int readVarInt(Connection* dst, int& bytes_read, int correction) {	//Декодер этих ебанных вар интов от Mojang
     int numRead = 0;
     int result = 0;
 	//++dst->packet_offset;
@@ -107,10 +109,10 @@ int readVarInt(Connection* dst, int& bytes_read, int correction) {
 
 
 
-
+//Декодер пакетов.
 int inspection(Connection* src, Connection* dst, BanList* ban_list, std::map <int,Connection*> *connectionList)
 {
-
+	
 	//THIS IS PACKET DECODER CODE
 	//DO NOT TOUCH ANYTHING!
 	
@@ -118,34 +120,36 @@ int inspection(Connection* src, Connection* dst, BanList* ban_list, std::map <in
 		return 0;
 	
 	
-	dst->packet_offset = dst->packet_len;
-	dst->packet_len = 0;
+	dst->packet_offset = dst->packet_len; //Если пакет пришел не полностью, то мы просто перемещаемся
+	//дальше, не читая его до конца (ибо пока это не нужно)
+	dst->packet_len = 0;	//обнуляем длину пакета, готовим переменную для след. чтения.
 	
-	while(dst->packet_offset < dst->len )
-	{
-		int bytes_read = 0;
-		int size = readVarInt(dst, bytes_read, 0);
-		int id = readVarInt(dst, bytes_read, 1);
+	while(dst->packet_offset < dst->len )		//Пока мы не дошли до конца полученного огрызка данных
+	{						//А может и целого пакета, тут как получиться.
+		
+		
+		int bytes_read = 0;			//Показывает, сколько байт заняла переменная типа VarInt на чтении
+		int size = readVarInt(dst, bytes_read, 0);	//Чтение размера
+		int id = readVarInt(dst, bytes_read, 1);	//Чтение ID (в первом пакете читает криво, и получает номер протокола)
 		//int protocol_version = readVarInt(dst, bytes_read, 0);
-		if(!src->is_handshaked && !src->is_server)
+		if(!src->is_handshaked && !src->is_server)		//Если это клиент и у него не было хендшейка
 		{
-			++dst->packet_offset;
+			++dst->packet_offset;				//Сдвигаемся на байт вперед и начинаем читать пакет
 			int nickname_len = readVarInt(dst, bytes_read,0);
 			dst->packet_offset += nickname_len;
-			
-			
 			dst->packet_offset += 2;
-			uint32_t next_state = readVarInt(dst, bytes_read, 0);
-			//std::cout << "Caught handshake id= " << id <<" state= " << next_state << std::endl;
+			uint32_t next_state = readVarInt(dst, bytes_read, 0);	//Получаем Next State поле пакета.
 			
-			if(next_state > 2 || next_state < 1 || id > 340 || id < 250)	//wrong handshake packet. Starting BAN MACHINE!
+			std::cout << "Caught handshake id= " << id <<" state= " << next_state << std::endl;
+			
+			if(next_state > 2 || next_state < 1 || id > 340 || id < 250)	//Если он кривой-косой, в печь нахуй!
 			{
 				ban_list->add(src->ip, 15);//ban ip addr for 15 minutes
 				close(src->src);
 				close(src->dst);
 				
 				
-				
+				//Удаляем данные о соединении из ОЗУ
 				connectionList->erase(src->src);
 				connectionList->erase(dst->src);
 				delete src;
@@ -153,20 +157,22 @@ int inspection(Connection* src, Connection* dst, BanList* ban_list, std::map <in
 				
 				return 0;
 				//Banned successfully nahuy
+				//Миссия выполнена
 			}
-			
+			//Если же пришел валидный хендшейк, ставим значение в true и перестаем читать трафик до обрыва этого соединения
 			src->is_handshaked = true;
 			return 0;
 		}
 		
-		dst->packet_offset += size - bytes_read;
+		dst->packet_offset += size - bytes_read;	//Пакет прочитан, сдвигаем оффсет на его длину.
 	}
 
 	
-	if(dst->packet_offset > dst->len)
+	if(dst->packet_offset > dst->len)				//Если же мы каким-то образом прочитали больше,
+									//чем было получено, то дочитываем из след. порции данных
 		dst->packet_len = dst->packet_offset - dst->len;
 	
-	dst->packet_offset = 0;
+	dst->packet_offset = 0;						//готовимся к новой порции данных, чистим оффсет.
 	
 	
 	return 0;
@@ -206,31 +212,31 @@ int main()
 	
 	
 	
-	struct epoll_event ev, events[MAX_EVENTS];
+	struct epoll_event ev, events[MAX_EVENTS];	//Создаем структуры еполла
 	
 	
 	
-	if (bind(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+	if (bind(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)		//Биндимся на порт
 		std::cout << "[ERROR] Binding error. Port " << port << " is busy" << std::endl;
 	else
 		std::cout << "[OK] Binding at port " << port << std::endl;
 	
 	
-	setnonblocking(sock);
-    	epollfd = epoll_create(1);
-    	ev.events = EPOLLIN;// | EPOLLET;
-    	ev.data.fd = sock;
+	setnonblocking(sock);		//Делаем сокет этого сервера неблокирующим
+    	epollfd = epoll_create(1);	//Получаем файловый дескриптор Epoll'а
+    	ev.events = EPOLLIN;		//Указываем, что с серверного сокета мы отслеживаем только события о доступности чтения
+    	ev.data.fd = sock;		//указываем сокет, который надо добавить в еполл
 
-	if(epoll_ctl(epollfd, EPOLL_CTL_ADD, sock, &ev) == -1)
+	if(epoll_ctl(epollfd, EPOLL_CTL_ADD, sock, &ev) == -1)		//вносим изменения.
         	std::cout << "[ERROR] epoll_ctl(): server's socket" << std::endl;
 	
 	
 
 	while(true)
 	{
-		int nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+		int nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);		//Ждем пока не появятся события
 
-        	if (nfds == -1) 
+        	if (nfds == -1) 					//Тут если че-то пойдет не так.
 		{
 			std::cout << "[ERROR] nfds error: Err_code=" << errno << std::endl;
 			return 0;
@@ -238,7 +244,7 @@ int main()
 		
 
 
-		for (register size_t k = 0; k < nfds; ++k)
+		for (register size_t k = 0; k < nfds; ++k)		//Начинаем обходить все полученные события.
 		{
 			if(events[k].data.fd == sock)							//если есть запрос на подключение, принимаем.
 			{            
@@ -248,7 +254,7 @@ int main()
                 		{
 					connection = accept(sock, (struct sockaddr*)&from, &len);
 					
-					if(ban_list.check(from.sin_addr.s_addr) > 0 )
+					if(ban_list.check(from.sin_addr.s_addr) > 0 )	//Если в бане - шлем далеко и надолго
 					{
 						//printf("[debug] here1, banned\n");
 						close(connection);
@@ -256,7 +262,7 @@ int main()
 						continue;
 					}
 					
-					if(time(0) > ban_list.check(from.sin_addr.s_addr))
+					if(time(0) > ban_list.check(from.sin_addr.s_addr))	//Если вышло время бана - разбаниваем
 					{
 						ban_list.remove(from.sin_addr.s_addr);
 					}
@@ -267,7 +273,12 @@ int main()
 					if (setnonblocking(connection) == -1)
 						printf("\033[01;31m[ERROR]\033[0m setnonblocking()\n");
 
+					//Далее опять добавляем сокеты на мониторинг в Epoll,
+					//ставим нужные типы событий на слежение.
 					
+					
+					
+					//	    Чтение    Дисконнект или ошибка   Запись
 					ev.events = EPOLLIN | EPOLLRDHUP | EPOLLERR | EPOLLET;
 					//ev.events = EPOLLRDHUP | EPOLLERR | EPOLLET | EPOLLOUT;
 					ev.data.fd = connection;
@@ -275,6 +286,9 @@ int main()
 					int server = socket_connect(main_server_ip.c_str(), main_server_port);
 
 					setnonblocking(server);
+					
+					//Ставим TCP NoDelay
+					
 					int yes = 1;
 					int result = setsockopt(server,
 								IPPROTO_TCP,
@@ -288,6 +302,7 @@ int main()
 								(char *) &yes, 
 								sizeof(int));
 					
+					//Ну тут создаем для них структуры, запихиваем в список...
 					
 					Connection* client_connection = new Connection();
 					client_connection->ip = from.sin_addr.s_addr;
@@ -306,11 +321,14 @@ int main()
 					connectionList.insert(std::pair<int, Connection*>(connection, client_connection));
 					
 					
+					//запихиваем новое входящее соединение на мониторинг в еполл
+					
 					if(epoll_ctl(epollfd, EPOLL_CTL_ADD, connection, &ev) == -1)
 						printf("\033[01;31m[ERROR]\033[0m epoll_ctl(connection) returned an error\n");
 					//else
 					//	printf("\033[01;33m[INFO]\033[0m New Client's IP: %s   [%d]\n", inet_ntoa(from.sin_addr), client_connection->src);
 		                    	
+					//И соединение до сервера туда же
 					ev.data.fd = server;
 					epoll_ctl(epollfd, EPOLL_CTL_ADD, server, &ev);
 				}
@@ -320,7 +338,7 @@ int main()
 			{
 
 				
-				if (events[k].events & EPOLLIN)
+				if (events[k].events & EPOLLIN)	
 				{
 					
 					if(connectionList.find(events[k].data.fd) != connectionList.end())
